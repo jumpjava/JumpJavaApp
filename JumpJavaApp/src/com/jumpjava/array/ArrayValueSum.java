8b6c920f28a58c1590d2ceb4ea160b18469be995
OM <OM@OM-PC>
2018-05-29 22:57:07 +0530

    Write a Java program to sum values of an array.

---
+++ JumpJavaApp/src/com/jumpjava/array/ArrayValueSum.java
@@ -0,0 +1,30 @@
+package com.jumpjava.array;
+/*
+ * Author : JumpJava.com
+ * JAVA : 1.7
+ * */
+
+public class ArrayValueSum {
+
+	public static void main(String[] args) {
+
+		int[] inputArray = { 1, 4, 5, 6, 7, 20 };
+
+		int result = ArrayValueSum.getSumOfArrayValue(inputArray);
+		System.out.println(result);
+	}
+
+	public static int getSumOfArrayValue(int[] array) {
+
+		int[] inputArray = array;
+		int sum = 0;
+
+		for (int i = 0; i < inputArray.length; i++) {
+
+			sum = sum + inputArray[i];
+
+		}
+		return sum;
+
+	}
+}
