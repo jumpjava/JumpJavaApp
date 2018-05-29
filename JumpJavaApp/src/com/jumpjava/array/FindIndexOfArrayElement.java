d6d4e869ad112769e0148fc7391d326e180aaa95
OM <OM@OM-PC>
2018-05-29 23:11:40 +0530

    . Write a Java program to find the index of an array element.

---
+++ JumpJavaApp/src/com/jumpjava/array/FindIndexOfArrayElement.java
@@ -0,0 +1,28 @@
+package com.jumpjava.array;
+
+/*
+ * Author : JumpJava.com
+ * JAVA : 1.7
+ * 
+ * */
+
+public class FindIndexOfArrayElement {
+
+	public static void main(String[] args) {
+
+		int[] inputArray = { 1, 4, 5, 6, 7, 20 };
+
+		FindIndexOfArrayElement.getIndexElementOfArray(inputArray);
+
+	}
+
+	public static void getIndexElementOfArray(int[] array) {
+
+		for (int i = 0; i < array.length; i++) {
+
+			System.out.println("Index Of an Array Element : " + array[i] + " is : " + i);
+		}
+
+	}
+
+}
