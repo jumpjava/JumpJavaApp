ca7d14c61ed9cb7ffcfd8214942cf6c23555e5a9
OM <OM@OM-PC>
2018-06-04 19:34:09 +0530

    15.Write a Java program to reverse an array of integer values.

--- JumpJavaApp/src/com/jumpjava/array/ReverseArrayElement.java
+++ JumpJavaApp/src/com/jumpjava/array/ReverseArrayElement.java
@@ -1,5 +1,7 @@
 package com.jumpjava.array;
 
+import java.util.Arrays;
+
 /*
  * Author  : JumpJava.com
  * JAVA    : 1.7
@@ -9,16 +11,22 @@ package com.jumpjava.array;
 public class ReverseArrayElement {
 
 	public static void main(String[] args) {
-		
+
 		int array[] = { 10, 46, 47, 20, 20, 52, 48, 36, 55, 95 };
+
+		int[] reverse=ReverseArrayElement.toReverseArray(array);
 		
-		ReverseArrayElement.toReverseArray(array);
+		System.out.println(Arrays.toString(reverse));
 	}
 
-	public static void toReverseArray(int[] array) {
+	public static int[] toReverseArray(int[] array) {
+		int[] reverse = new int[array.length];
 
-		for (int i = array.length-1; i >= 0; i--) {
-			System.out.print(array[i]+" ");
+		for (int i = array.length - 1; i >= 0; i--) {
+			reverse[i] = array[i];
 		}
+
+	
+		return reverse;
 	}
 }
