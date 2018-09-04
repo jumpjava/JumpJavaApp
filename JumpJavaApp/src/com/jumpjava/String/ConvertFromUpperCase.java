be700c7734ff1176572fe093d35b20f6bbc9cdd7
OM <OM@OM-PC>
2018-09-04 23:45:59 +0530

    Program to convert all the characters in a string to Upper case.

---
+++ JumpJavaApp/src/com/jumpjava/String/ConvertFromUpperCase.java
@@ -0,0 +1,23 @@
+/*
+ * Author  : JumpJava.com
+ * JAVA    : 1.7
+ * Program : 18.Write a Java program to convert all the characters in a string to Upper case.  
+ * */
+package com.jumpjava.String;
+
+public class ConvertFromUpperCase {
+
+	public static void main(String[] args) {
+
+		String str = "i love jump java";
+		ConvertFromUpperCase.getLowerCase(str);
+	}
+
+	public static void getLowerCase(String str) {
+		String inputString=str;
+		String output=inputString.toUpperCase();
+		System.out.println("Input String -> "+inputString);
+		System.out.println("Output String -> "+output);
+
+	}
+}
