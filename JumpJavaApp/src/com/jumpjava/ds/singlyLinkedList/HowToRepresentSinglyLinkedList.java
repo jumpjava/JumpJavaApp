c1db1a94d45bc899c13b96d662bdb96c713738a7
OM <OM@OM-PC>
2018-09-12 00:28:45 +0530

    1.How to represent a Singly Linked List in Java.

--- JumpJavaApp/src/com/jumpjava/ds/singlyLinkedList/HowToRepresentSinglyLinkedList.java
+++ JumpJavaApp/src/com/jumpjava/ds/singlyLinkedList/HowToRepresentSinglyLinkedList.java
@@ -7,7 +7,31 @@ package com.jumpjava.ds.singlyLinkedList;
 
 public class HowToRepresentSinglyLinkedList {
 
+	/* Inner class to represent the Linked List */
+	private static class ListNode {
+
+		/*
+		 * It has two private member one is data and another one is the address
+		 * of the next node
+		 */
+		private int data;
+		private ListNode next;
+
+		/*
+		 * It has the one constructor which is responsible to initialize the
+		 * node/data and the address of the next node. Since by default the
+		 * address in null for next node
+		 */
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
 	public static void main(String[] args) {
-		//toDO
+
+		/*First node of the Singly Linked List*/
+		ListNode node = new ListNode(1);
+
 	}
 }
