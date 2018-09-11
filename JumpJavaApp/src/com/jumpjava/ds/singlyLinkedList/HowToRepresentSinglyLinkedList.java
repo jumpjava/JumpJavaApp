c798948d5f8aea52526d156aafb2507de9bf07ea
OM <OM@OM-PC>
2018-09-12 00:16:43 +0530

    1.How to represent a Singly Linked List in Java.

---
+++ JumpJavaApp/src/com/jumpjava/ds/singlyLinkedList/HowToRepresentSinglyLinkedList.java
@@ -0,0 +1,13 @@
+/*
+ * Author  : JumpJava.com
+ * JAVA    : 1.7
+ * Program : 1.How to represent a Singly Linked List in Java.
+ * */
+package com.jumpjava.ds.singlyLinkedList;
+
+public class HowToRepresentSinglyLinkedList {
+
+	public static void main(String[] args) {
+		//toDO
+	}
+}
