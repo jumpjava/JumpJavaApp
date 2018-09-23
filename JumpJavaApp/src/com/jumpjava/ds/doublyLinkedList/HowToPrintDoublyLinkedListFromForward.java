ab637df99e2bf2796d24e97cb0d00453f4b68f63
OM <OM@OM-PC>
2018-09-23 16:40:53 +0530

    4.Print the element of DLL from forward direction in java.

---
+++ JumpJavaApp/src/com/jumpjava/ds/doublyLinkedList/HowToPrintDoublyLinkedListFromForward.java
@@ -0,0 +1,82 @@
+/*
+ * Author  : JumpJava.com
+ * JAVA    : 1.7
+ * Program : 4.How to print the element of Doubly Linked List from forward direction in java.
+ * */
+
+package com.jumpjava.ds.doublyLinkedList;
+
+public class HowToPrintDoublyLinkedListFromForward {
+
+	private ListNode head;
+	private ListNode tail;
+	private int length;
+
+	public HowToPrintDoublyLinkedListFromForward() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	private static class ListNode {
+
+		private int data;
+		private ListNode next;
+		private ListNode previous;
+
+		public ListNode(int data) {
+			this.data = data;
+		}
+
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public void insertLast(int value) {
+
+		ListNode newNode = new ListNode(value);
+
+		if (isEmpty()) {
+			head = newNode;
+		} else {
+			tail.next = newNode;
+		}
+		newNode.previous = tail;
+		tail = newNode;
+		length++;
+
+	}
+
+	public void displayForward() {
+		if (head == null) {
+			return;
+		} else {
+			ListNode temp = head;
+			while (temp != null) {
+				System.out.print(temp.data + "=>");
+				temp = temp.next;
+			}
+			System.out.println(temp);
+		}
+	}
+
+
+	public static void main(String[] args) {
+
+		HowToPrintDoublyLinkedListFromForward dll = new HowToPrintDoublyLinkedListFromForward();
+		dll.insertLast(11);
+		dll.insertLast(21);
+		dll.insertLast(31);
+		dll.insertLast(41);
+		dll.insertLast(51);
+		System.out.println("Print the Element from Forward direction");
+		dll.displayForward();
+	}
+
+}
